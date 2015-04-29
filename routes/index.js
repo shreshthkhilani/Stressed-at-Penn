a0e361d2fdb8b9ede72b1c58856a060de14fbb67
Shreshth Khilani <shreshth.khilani@gmail.com>
2015-04-29 03:14:56 -0400

    recptcha

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,29 @@ exports.init = function(ad,hr,ph,ct,pd,callback) {
 
 async = require("async");
 
+function recaptcha_verify(response, remoteip, callback) {
+	var fs = require('fs');
+	var recap = JSON.parse(fs.readFileSync('recap.json', 'utf8'));
+	var recap_private_key = recap.private_key;
+	var recap_public_key = recap.public_key;
+
+	var request = require('request');
+	request.post(
+		'https://www.google.com/recaptcha/api/siteverify', 
+		{
+			form: {
+			    secret: recap_private_key,
+			    response: response,
+			    remoteip: remoteip
+			},
+		    json: true
+		}, 
+		function (err, res, body) {
+			console.log(res.body);
+			callback(res.body.success);
+		}
+	);	
+}
 
 exports.index = function(req, res) {
 	var t = 'Submit';
@@ -46,103 +69,116 @@ exports.index = function(req, res) {
 };
 
 exports.submitPhoto = function(req, res) {
-	if (req.files.displayImage == undefined) {
-		res.redirect("/");
-		return;
-	}
-	//console.log(req);
-	//console.log(req.files);
-	var fs = require('fs');
-	var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+	console.log(req.connection.remoteAddress);
+	console.log(req.body['g-recaptcha-response']);
 
-	var uuid = require('node-uuid');
-	var file_suffix = uuid.v1()
-	/*var Upload = require('s3-uploader');
-	var client = new Upload('stressed-at-penn', {
-  		awsBucketUrl: 'https://s3.amazonaws.com/stressed-at-penn/',
-  		awsBucketPath: 'images/',
-  		awsBucketAcl: 'public-read',
-  		versions: [{
-		    suffix: file_suffix,
-		    quality: 80,
-		    maxHeight: 1040,
-		    maxWidth: 1040,
-  		}]
-	});*/
-	var s3 = require('s3');
-	var client = s3.createClient({
-	  maxAsyncS3: 20,     // this is the default 
-	  s3RetryCount: 3,    // this is the default 
-	  s3RetryDelay: 1000, // this is the default 
-	  multipartUploadThreshold: 20971520, // this is the default (20 MB) 
-	  multipartUploadSize: 15728640, // this is the default (15 MB) 
-	  s3Options: {
-	    accessKeyId: obj.accessKeyId,
-	    secretAccessKey: obj.secretAccessKey,
-	    // any other options are passed to new AWS.S3() 
-	    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
-	  },
-	});
-
-	var params = {
-	  localFile: req.files.displayImage.path,
-	 
-	  s3Params: {
-	    Bucket: "stressed-at-penn",
-	    Key: "images/" + file_suffix,
-	    // other options supported by putObject, except Body and ContentLength. 
-	    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
-	  },
-	};
-	var uploader = client.uploadFile(params);
-	uploader.on('error', function(err) {
-	  console.error("unable to upload:", err.stack);
-	});
-	uploader.on('progress', function() {
-	  console.log("progress", uploader.progressMd5Amount,
-	            uploader.progressAmount, uploader.progressTotal);
-	});
-	uploader.on('end', function() {
-	  console.log("done uploading");
-	/*});
-
-	client.upload(req.files.displayImage.path, {}, function(err, images, meta) {
-	  if (err) {
-	    console.error(err);
-	  } else {*/
-	    //console.log(images);
-	    //console.log(meta);
-		//fs.unlinkSync(req.files.displayImage.path);
-
-		var image_url = 'https://s3.amazonaws.com/stressed-at-penn/images/' + file_suffix;// + images[0].path;
-		var date = new Date();
-		var month = date.getMonth() + 1;
-		var hour = month.toString() + "-" + date.getDate().toString() + "-" + date.getHours().toString();
-		var pId = photos.inx.toString();
-		var json_data = {
-			"timestamp": date,
-			"pId": pId,
-			"url" : image_url
-		};
-		console.log(json_data);
-		console.log(pId);
-		photos.put(pId, JSON.stringify(json_data), function(err,data) {
-			if (err) {
-				console.log("Error photo upload 1");
+	recaptcha_verify(req.body['g-recaptcha-response'], req.connection.remoteAddress, 
+		function(truthVal) {
+			if (!truthVal) {
+				res.redirect("/");
+				return;
 			}
 			else {
-				hours.addToSet(hour, pId, function(err2, data2) {
-					if (err2) {
-						console.log("Error photo upload 2");
-					}
-					else {
-						res.redirect("/view");	
-					}
+				if (req.files.displayImage == undefined) {
+					res.redirect("/");
+					return;
+				}
+				//console.log(req);
+				//console.log(req.files);
+				var fs = require('fs');
+				var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+				var uuid = require('node-uuid');
+				var file_suffix = uuid.v1()
+				/*var Upload = require('s3-uploader');
+				var client = new Upload('stressed-at-penn', {
+			  		awsBucketUrl: 'https://s3.amazonaws.com/stressed-at-penn/',
+			  		awsBucketPath: 'images/',
+			  		awsBucketAcl: 'public-read',
+			  		versions: [{
+					    suffix: file_suffix,
+					    quality: 80,
+					    maxHeight: 1040,
+					    maxWidth: 1040,
+			  		}]
+				});*/
+				var s3 = require('s3');
+				var client = s3.createClient({
+				  maxAsyncS3: 20,     // this is the default 
+				  s3RetryCount: 3,    // this is the default 
+				  s3RetryDelay: 1000, // this is the default 
+				  multipartUploadThreshold: 20971520, // this is the default (20 MB) 
+				  multipartUploadSize: 15728640, // this is the default (15 MB) 
+				  s3Options: {
+				    accessKeyId: obj.accessKeyId,
+				    secretAccessKey: obj.secretAccessKey,
+				    // any other options are passed to new AWS.S3() 
+				    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
+				  },
+				});
+
+				var params = {
+				  localFile: req.files.displayImage.path,
+				 
+				  s3Params: {
+				    Bucket: "stressed-at-penn",
+				    Key: "images/" + file_suffix,
+				    // other options supported by putObject, except Body and ContentLength. 
+				    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
+				  },
+				};
+				var uploader = client.uploadFile(params);
+				uploader.on('error', function(err) {
+				  console.error("unable to upload:", err.stack);
+				});
+				uploader.on('progress', function() {
+				  console.log("progress", uploader.progressMd5Amount,
+				            uploader.progressAmount, uploader.progressTotal);
+				});
+				uploader.on('end', function() {
+				  console.log("done uploading");
+				/*});
+
+				client.upload(req.files.displayImage.path, {}, function(err, images, meta) {
+				  if (err) {
+				    console.error(err);
+				  } else {*/
+				    //console.log(images);
+				    //console.log(meta);
+					//fs.unlinkSync(req.files.displayImage.path);
+
+					var image_url = 'https://s3.amazonaws.com/stressed-at-penn/images/' + file_suffix;// + images[0].path;
+					var date = new Date();
+					var month = date.getMonth() + 1;
+					var hour = month.toString() + "-" + date.getDate().toString() + "-" + date.getHours().toString();
+					var pId = photos.inx.toString();
+					var json_data = {
+						"timestamp": date,
+						"pId": pId,
+						"url" : image_url
+					};
+					console.log(json_data);
+					console.log(pId);
+					photos.put(pId, JSON.stringify(json_data), function(err,data) {
+						if (err) {
+							console.log("Error photo upload 1");
+						}
+						else {
+							hours.addToSet(hour, pId, function(err2, data2) {
+								if (err2) {
+									console.log("Error photo upload 2");
+								}
+								else {
+									res.redirect("/view");	
+								}
+							});
+						}
+					});
+				  //}
 				});
 			}
-		});
-	  //}
-	});
+		}
+	);
 };
 
 exports.view = function(req, res) {
@@ -195,7 +231,7 @@ exports.getGrid = function(req, res) {
 		},
 		function() {
 			returnDataPre = returnDataPre.sort(function(a,b){return b-a;});
-			console.log(returnDataPre);
+			//console.log(returnDataPre);
 			i = 0;
 			async.until(
 				function() {
@@ -204,10 +240,10 @@ exports.getGrid = function(req, res) {
 				function(callback) {
 					var current = returnDataPre[i];
 					i++;
-					console.log(current);
+					//console.log(current);
 					photos.get(current.toString(), function(err,data) {
 						if (err) {
-							console.log("Error here");
+							//console.log("Error here");
 							callback();
 						}
 						else {
